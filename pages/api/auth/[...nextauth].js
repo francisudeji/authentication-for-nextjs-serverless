c000e882782a6fb72e0455d3e28c83f8cd0244a3
francisudeji <francis.udejiofficial@gmail.com>
2020-08-17 12:53:54 +0100

    Authenticate with email

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,26 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+
+const options = {
+  site: process.env.NEXTAUTH_URL,
+  providers: [
+    Providers.Email({
+      server: {
+        port: 465,
+        host: 'smtp.gmail.com',
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  database: process.env.DATABASE_URL,
+}
+
+export default (req, res) => NextAuth(req, res, options)
